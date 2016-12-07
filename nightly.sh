4d985e9001c5ea28081c0d5aa952243009159415
Joseph Blodgett <Joseph.blodgett@gmail.com>
2016-12-07 11:38:56 -0700

    fixes to the incremental backups

--- nightly.sh
+++ nightly.sh
@@ -3,8 +3,8 @@
 # directories to backup
 # for each directory to be backed up you must input an output directory,
 ODIR=(
-/mnt/observe/backups/$HOSTNAME/www
-/mnt/observe/backups/$HOSTNAME/content)
+/mnt/observe/backups/$HOSTNAME/content
+/mnt/observe/backups/$HOSTNAME/www)
 
 BDIR=(
 /usr/local/WowzaStreamingEngine/content
@@ -13,22 +13,35 @@ BDIR=(
 LOGDIR=/usr/sbin/backups/logs
 ########################################################################
 
+if [ ! -d $LOGDIR ]
+then
+	mkdir $LOGDIR
+fi
+
 if [ ${#BDIR[@]} -ne ${#ODIR[@]} ];
 then
 	exit -1
 fi	
 
-BACKUPDIR=`date +%Y-%m-%d:%H:%M:%S` 
-OPTS="--force --delete --backup --backup-dir=$ODIR/incremental/$BACKUPDIR -avz"
+BACKUPDIR=`date +%Y-%m-%d:%H:%M:%S`
 
 export PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 VAR=0
 
 for P in ${BDIR[@]}
 do
+	INCREMENTALDIR=${ODIR[$var]}/incremental/$BACKUPDIR
+	OPTS="--force --delete --backup --backup-dir=$INCREMENTALDIR -avz"
+
 	#echo $P
 	echo ${ODIR[$var]}
+	echo "-------------$P >> ${ODIR[$var]}/current--------------" >> $LOGDIR/$BACKUPDIR.txt
+	echo "-------------Inrementals >> $INCREMENTALDIR ----------------" >> $LOGDIR/$BACKUPDIR.txt
+	echo "" >> $LOGDIR/$BACKUPDIR.txt
+
       	# transfer
-      	rsync $OPTS $P ${ODIR[$var]}/Current | $LOGDIR/$BACKUPDIR.txt
+      	rsync $OPTS $P ${ODIR[$var]}/Current >> $LOGDIR/$BACKUPDIR.txt
+	echo "" >> $LOGDIR/$BACKUPDIR.txt
+	
 	((var++))      	
 done
